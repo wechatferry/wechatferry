568dbf58b0d8fcf578ae02e1845a6e14d68edb19
kejun <kejun1997@gmail.com>
2024-09-07 12:41:37 +0800

    feat(plugins)!: add safe-mode plugin

--- packages/nuxt/src/runtime/server/utils/useBotPuppet.ts
+++ packages/nuxt/src/runtime/server/utils/useBotPuppet.ts
@@ -1,12 +1,17 @@
 import { WechatferryPuppet } from '@wechatferry/puppet'
+import { createSafeModePuppet } from '@wechatferry/plugins'
 import { useBotAgent } from './useBotAgent'
 
 let puppet: WechatferryPuppet
 
 export function useBotPuppet() {
   const agent = useBotAgent()
+  const { wcferry: { safeMode } } = useRuntimeConfig()
   if (!puppet) {
     puppet = new WechatferryPuppet({ agent })
+    if (safeMode) {
+      puppet = createSafeModePuppet(puppet)
+    }
   }
 
   return puppet
---
+++ packages/plugins/src/index.ts
@@ -0,0 +1,1 @@
+export * from './safe-mode/safe-mode'
