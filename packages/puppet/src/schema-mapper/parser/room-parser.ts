48e9a6778406e116bc8b729816dc2167662c9a86
kejun <kejun1997@gmail.com>
2024-09-08 19:29:28 +0800

    fix(puppet): room atUserList parser

--- packages/puppet/src/schema-mapper/parser/room-parser.ts
+++ packages/puppet/src/schema-mapper/parser/room-parser.ts
@@ -9,10 +9,10 @@ export const roomParser: MessageParser = async (message: WxMsg, ret: PuppetMessa
   if (message.is_group) {
     context.isRoomMessage = true
     try {
-      const xml = await xmlToJson<{ msgsource?: { atuserlist?: string | string[] } }>(message.xml)
+      const xml = await xmlToJson<{ msgsource?: { atuserlist?: string } }>(message.xml)
       if (xml?.msgsource?.atuserlist) {
-        const atUserList = Array.isArray(xml.msgsource.atuserlist) ? xml.msgsource.atuserlist : [xml.msgsource.atuserlist]
-        const mentionIdList = atUserList.map(v => v.trim()).filter(v => v).filter(v => v !== ',')
+        const atUserList = xml.msgsource.atuserlist
+        const mentionIdList = atUserList.split(',').map(v => v.trim()).filter(v => v)
         if (mentionIdList.length) {
           log.verbose('roomParser', `mentionIdList: ${mentionIdList}`)
           const room = ret as PUPPET.payloads.MessageRoom
