7714c9853dc2bb4210cbcfa8fc0c8446c1170331
LuChao <atorber@163.com>
2024-12-13 20:17:53 +0800

    feat: add initial puppet example with package.json and index.ts

---
+++ examples/puppet/src/index.ts
@@ -0,0 +1,16 @@
+import { WechatferryPuppet } from 'wechatferry/puppet'
+import { WechatyBuilder } from 'wechaty'
+import { useLogger } from 'wechatferry/logger'
+
+const logger = useLogger('agent-example')
+
+const puppet = new WechatferryPuppet()
+const bot = WechatyBuilder.build({ puppet })
+
+bot.on('message', (msg) => {
+  logger.info(JSON.stringify(msg, null, 2))
+  msg.text() === 'ding' && msg.say('dong')
+})
+  .start()
+  .then(() => logger.info('Bot started'))
+  .catch(console.error)
